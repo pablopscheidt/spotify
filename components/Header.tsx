8fa2bbbc8ff9d991c5a491424c46d2d5e9b1f4d8
Pablo Boing Pscheidt <nikebryt@gmail.com>
2023-09-28 23:58:53 -0300

    Initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
+import { HiHome } from "react-icons/hi";
+
+interface HeaderProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Header: React.FC<HeaderProps> = (children, className) => {
+    const router = useRouter();
+
+    const handleLogout = () => {
+
+    }
+
+    return ( 
+        <div className={twMerge(`
+            h-fit
+            bg-gradient-to-b
+            from-emerald-800
+            p-6
+        `,
+            className
+        )}>
+            <div className="w-full mb-4 flex items-center justify-between">
+                <div className="hidden md:flex gap-x-2 items-center">
+                    <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                        <RxCaretLeft size={35} className="text-white" />
+                    </button>
+
+                    <button onClick={() => router.forward()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                        <RxCaretRight size={35} className="text-white" />
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button>
+                        <HiHome />
+                        {/* 36.54 */}
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Header;
